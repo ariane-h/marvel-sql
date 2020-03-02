66a4ef8140be606c35ec9044181a4bf605c5ff9e
Ariane Holford <aribomop@gmail.com>
2020-03-02 14:47:54 +0000

    tidied up

--- marvel.sql
+++ marvel.sql
@@ -78,9 +78,9 @@ INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:0
 -- SELECT * FROM people;
 
 -- The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! Create a new entry in the 'movies' table to reflect this.
-SELECT * FROM movies;
-INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2018, '16:00');
-SELECT * FROM movies;
+-- SELECT * FROM movies;
+-- INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2018, '16:00');
+-- SELECT * FROM movies;
 
 -- The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show time of "Iron Man 3" to start two hours later.
 SELECT show_time FROM movies WHERE title = 'Iron Man 2';
